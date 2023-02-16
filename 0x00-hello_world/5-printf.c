8c8a2ef1424a5192d9febe3492d66dcf5ade5240
thewodros9 <teddy2002.seyoum@gmail.com>
2023-02-16 12:35:26 -0800

    5-print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - A program that print line with printf function
+ * Return: 0 (success)
+ */
+int  main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
