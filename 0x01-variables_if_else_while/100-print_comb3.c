00589f6adb201490701b945ad5b7d4cf9a77c928
thewodros9 <teddy2002.seyoum@gmail.com>
2023-02-17 05:00:13 -0800

    100-print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 1; b < 10; b++)
+		{
+			if (a < b)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
