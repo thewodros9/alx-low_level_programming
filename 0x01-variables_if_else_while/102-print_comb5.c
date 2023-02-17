e292772eb7caf8c187a0e5b5aa2d361878012230
thewodros9 <teddy2002.seyoum@gmail.com>
2023-02-17 07:07:28 -0800

    102-print

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c = 0;
+	int a;
+	int b;
+	int c2;
+	int e;
+	int f;
+
+	while (c <= 98)
+	{
+		a = (c / 10 + '0');
+		b = (c % 10 + '0');
+		c2 = 0;
+		while (c2 <= 99)
+		{
+			e = (c2 / 10 + '0');
+			f = (c2 % 10 + '0');
+			if (c < c2)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(' ');
+				putchar(e);
+				putchar(f);
+				if (c != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			c2++
+		}
+		c++
+	}
+	putchar('\n');
+	return (0);
+}
