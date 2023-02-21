3c60a8fcafa7a6b13fc7c191cb27b6d8de491d86
thewodros9 <teddy2002.seyoum@gmail.com>
2023-02-21 12:49:32 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,7 +12,7 @@ int main(void)
 	while (i < 50)
 	{
 		if (i == 0)
-			printf("ld", j);
+			printf("%ld", j);
 		else if (i == 1)
 			printf(", %ld", k);
 		else
@@ -23,6 +23,6 @@ int main(void)
 		}
 		++i;
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
