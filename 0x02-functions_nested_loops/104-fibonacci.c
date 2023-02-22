22e74fa273bac877c758f24fa58e2159769ce2f1
thewodros9 <teddy2002.seyoum@gmail.com>
2023-02-22 01:25:34 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_hair1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 
 	for (count = 0; count < 92; count++)
 	{
