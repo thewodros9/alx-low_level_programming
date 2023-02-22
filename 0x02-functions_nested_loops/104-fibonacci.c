c10d9c5459e0852fdf633e2ef6faa49705995d7c
thewodros9 <teddy2002.seyoum@gmail.com>
2023-02-22 01:47:41 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,41 +5,41 @@
  */
 int main(void)
 {
-	int count;
+	int i;
 	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+	unsigned long f1_h1, f1_h2, f2_h1, f2_h2;
+	unsigned long h1, h2;
 
-	for (count = 0; count < 92; count++)
+	for (i = 0; i < 92; i++)
 	{
 		sum = fib1 + fib2;
 		printf("%lu, ", sum);
 		fib1 = fib2;
 		fib2 = sum;
 	}
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
-	for (count = 93; count < 99; count++)
+	f1_h1 = fib1 / 10000000000;
+	f2_h1 = fib2 / 10000000000;
+	f1_h2 = fib1 % 10000000000;
+	f2_h2 = fib2 % 10000000000;
+	for (i = 93; i < 99; i++)
 	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
+		h1 = f1_h1 + f2_h1;
+		h2 = f1_h2 + f2_h2;
+		if (f1_h2 + f2_h2 > 9999999999)
 		{
-			half1 += 1;
-			half2 %= 10000000000;
+			h1 += 1;
+			h2 %= 10000000000;
 		}
-		printf("%lu%lu", half1, half2);
-		if (count != 98)
+		printf("%lu%lu", h1, h2);
+		if (i != 98)
 		{
 			printf(",");
 			putchar(' ');
 		}
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
+		f1_h1 = fib2_half1;
+		f1_h2 = f2_h2;
+		f2_h1 = h1;
+		f2_h2 = h2;
 	}
 	printf("\n");
 	return (0);
