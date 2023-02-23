6ea5091257e4c814b887e7e1220cb2e05191f8c9
thewodros9 <teddy2002.seyoum@gmail.com>
2023-02-23 03:47:55 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - print the number 0 -9
+ * Return: the number since 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	for (; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x);
+		}
+	}
+	_putchar('\n');
+}
+
