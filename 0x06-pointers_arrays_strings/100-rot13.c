4ab8f962158f3f31496ee75db38c8d11ccd1118f
thewodros9 <teddy2002.seyoum@gmail.com>
2023-03-02 01:05:38 -0800

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char d1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char d2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == d1[j])
+			{
+				s[i] = d2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
