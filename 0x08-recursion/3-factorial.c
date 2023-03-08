d86e57c334b1d7500894b6164d0767e37eab3a15
thewodros9 <teddy2002.seyoum@gmail.com>
2023-03-08 07:34:44 -0800

    int factorial(int n)

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - fuction to find factorial
+ * @n: input
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1));
+}
