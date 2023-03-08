6fd4ca1088d1525817db3ae08aa9c99ebdd474a7
thewodros9 <teddy2002.seyoum@gmail.com>
2023-03-08 08:45:32 -0800

    is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -22,7 +22,7 @@ int pl(char *s, int l)
 		return (1);
 	if (*s == *(s + 1))
 		return (pl(s + 1, l - 2));
-	return (0);
+	return (1);
 }
 /**
  * is_palindrome - palindrome
@@ -33,5 +33,5 @@ int is_palindrome(char *s)
 {
 	int l = strl(s);
 
-	return (pl(s, l - 2));
+	return (pl(s, l - 1));
 }
