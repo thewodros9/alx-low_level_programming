2e966544a61484560342da2ec6f6bc38dc1c7957
thewodros9 <teddy2002.seyoum@gmail.com>
2023-03-08 08:39:22 -0800

    is_palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * strl - size
+ * @s: pointer
+ * Return: recursion
+ */
+int strl(char *s)
+{
+	if (!*s)
+		return (0);
+	return (1 + strl(++s));
+}
+/**
+ * pl - palindrome
+ * @s: pointer
+ * @l:position
+ * Return: boolena
+ */
+int pl(char *s, int l)
+{
+	if (l < 1)
+		return (1);
+	if (*s == *(s + 1))
+		return (pl(s + 1, l - 2));
+	return (0);
+}
+/**
+ * is_palindrome - palindrome
+ * @s: pointer
+ * Return: recusion
+ */
+int is_palindrome(char *s)
+{
+	int l = strl(s);
+
+	return (pl(s, l - 1));
+}
