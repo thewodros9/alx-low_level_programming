8a7eb58a96545fa73613ad6ba4b5ae297b26305f
thewodros9 <teddy2002.seyoum@gmail.com>
2023-03-10 06:47:41 -0800

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _puts - function to print string
+ * @s: input
+ * Return: Awalys 0
+ */
+void _puts(char *s)
+{
+	puts(s);
+}
+
