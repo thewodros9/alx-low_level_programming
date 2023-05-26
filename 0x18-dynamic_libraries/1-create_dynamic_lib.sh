80acacecdf0a26342abb349ffe2b727a5519e575
thewodros9 <teddy2002.seyoum@gmail.com>
2023-05-26 10:37:28 -0700

    update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Wextra -werror -pendantic -c -fPIC *.c
+gcc -Wall -Wextra -werror -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
